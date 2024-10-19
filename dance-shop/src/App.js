27cebdd02b8551e418b4418816c559656ad09d6d
tom <stas15@stud.uniza.sk>
2024-10-19 13:16:45 +0200

    Pridany bootstrap

--- dance-shop/src/App.js
+++ dance-shop/src/App.js
@@ -3,18 +3,33 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import ProductsPage from './components/ProductsPage';
 import ContactPage from './components/ContactPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
     return (
         <Router>
             <div className="App">
-                <nav>
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/products">Products</Link></li>
-                        <li><Link to="/contact">Contact</Link></li>
-                    </ul>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <div className="container-fluid">
+                        <Link className="navbar-brand" to="/">Dance Shop</Link>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/">Home</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/products">Products</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/contact">Contact</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </nav>
                 <Routes>
                     <Route path="/" element={<HomePage />} />
